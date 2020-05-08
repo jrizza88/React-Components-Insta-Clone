529190698dbabf62ef20b0fbd9dfc7cad5be59b3
Jamar Torres <Jetorres88@gmail.com>
2020-05-07 22:32:40 -0400

    imported PostsPage and SearchBar components in App Component and added the likes props to the Like Section Component

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -6,6 +6,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons'
 
 const LikeSection = props => {
+  console.log('LikeSection props', props)
+  console.log('props.likes', props.likes)
   return (
     <div>
       <div
@@ -19,7 +21,7 @@ const LikeSection = props => {
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className="like-number">27 likes</p>
+      <p className="like-number">{props.likes}</p>
     </div>
   )
 };
